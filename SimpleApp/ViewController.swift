3253bcd80986020492d8bbc0239d146cdc0ec4de
nicopuri <nicospuri@gmail.com>
2019-03-26 00:04:09 -0300

    Testing

--- SimpleApp/ViewController.swift
+++ SimpleApp/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
-
+        // Testing
     }
 
     override func viewWillAppear(_ animated: Bool) {
