46ae2967d15a5364c24a11fa1834d6a4894fc6fa
Nicolas Purita <nicospuri@gmail.com>
2019-03-31 22:58:42 -0300

    Fixed swiftlint

--- SimpleApp/ViewController.swift
+++ SimpleApp/ViewController.swift
@@ -12,18 +12,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
     }
 
     deinit {
-        
     }
 
 }
-
